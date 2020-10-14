4fd8acbc988c5b7f7b3e9e4aab5f8196f5d65108
cct663 <cct663@gmail.com>
2020-10-13 21:36:21 -0400

    Fixed issue with the figure displaying in markdown. Made some edits to wording. Added example for displaying a fit model table in markdown using sjplot.

--- 3_r_scripts/1_main_script.R
+++ 3_r_scripts/1_main_script.R
@@ -4,13 +4,13 @@
 # parts of the project folder are organized the way they are.
 #
 # Written by: Sabrina McNew & Conor Taff
-# Last updated: 9/1/2020
+# Last updated: 10/13/2020
 # Run under R Studio 1.1.463 on Mac OSX 10.11.6
 # See markdown file for saving session info. If you aren't working in markdown it is also 
 # good practice to record package versions here.
 
 # Load packages ----
-  # Ending the line above with four dashes of four hashtags puts the chunk into your outline below. 
+  # Ending the line above with four dashes or four hashtags puts the chunk into your outline below. 
   # This makes it a lot easier to find things later in a long script. Another way to create sections 
   # is with "command + shift + R" in Rstudio. 
 
@@ -18,9 +18,10 @@
   # what is required and you are less likely to break your code when modifying it by moving a line
   # up to before the required package was loaded. I have a few packages I almost always load, but most
   # of these would change each time depending on the project. Just remember to add packages to the list
-  # up here when you want to use them.
-    pacman::p_load(tidyverse, here, plyr)
-      # 'here' is useful for refering to relative path names in your project directory.
+  # up here when you want to use them. You'll get an error if these packages aren't installed on your local
+  # machine yet and will need to run install.packages(xxx)
+    pacman::p_load(tidyverse, here, plyr, sjPlot)
+      # 'here' is useful for referring to relative path names in your project directory.
 
 # Load data ----
   # here is where I would load any raw data that you have added to the project
@@ -57,7 +58,9 @@
           write.table(full_hero_data, here::here("2_modified_data/full_hero_data.txt"), sep = "\t")
         
         # You could also save the thing you made as an r object instead that you can load again later. This is 
-          # especially useful for things like large models etc that can't be written to a plain column.
+          # especially useful for things like large models etc that can't be written to a plain column. For microbiome
+          # or diet analyses, after running the full pipeline you can save the final objects and then just reload
+          # them to pickup where you left off rather than having to run the full pipeline again.
           saveRDS(full_hero_data, here::here("5_other_outputs/full_hero_data.rds"))
           full_hero_data <- readRDS(here::here("5_other_outputs/full_hero_data.rds"))
     
@@ -69,10 +72,21 @@
             ggplot(aes(x = Height, y = Weight, color = Alignment)) +
             geom_point() + facet_wrap(~ Gender)
           
-          ggsave(here::here("4_output_figures/hero_figure.pdf"), device = "pdf")
+        # save here if you want to include in your markdown summary  
+          # note that if your markdown output is html, saving as a pdf doesn't work well
+          ggsave(here::here("3_r_scripts/hero_figure.png"), device = "png", width = 8, height = 4.5, units = "in")
+        # save here if you want to just keep the figure to add into a manuscript, etc, later
+          ggsave(here::here("4_output_figures/hero_figure.pdf"), device = "pdf") 
           
           
-          
-
-
+    # Fit a simple model and save the table
+          fhd2 <- subset(full_hero_data, full_hero_data$Alignment == "good" |
+                           full_hero_data$Alignment == "bad" |
+                           full_hero_data$Alignment == "neutral")
+          fhd2 <- subset(fhd2, fhd2$Gender == "Female" | fhd2$Gender == "Male")
+          m <- glm(Height ~ Alignment + Gender, data = fhd2, na.action = na.exclude)
+          m2 <- glm(Weight ~ Alignment + Gender, data = fhd2, na.action = na.exclude)
+          save_table <- tab_model(m, m2, 
+                    pred.labels = c("Intercept (align evil; female)", "Alignment Good", "Alignment Neutral", "Gender (male)"))
+          saveRDS(save_table, here::here("5_other_outputs/hero_table.rds"))
     
